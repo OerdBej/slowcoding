342057d43156c1166e88af5174c00665558a3b0f
oerd bej <oerdbej@gmail.com>
2024-07-15 10:56:17 +0200

    starting blog boilerplate

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>slowcoding</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/OerdBej',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/OerdBej',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'here the footer',
   },
 }
 
